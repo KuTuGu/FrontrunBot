b5675d32400da791f1e861987ffe48bc5e86e385
YuFei Wang <zhongliwang48@gmail.com>
2022-12-03 15:42:22 +0800

    refactor: restrict exec permission

--- src/Arbitrage.sol
+++ src/Arbitrage.sol
@@ -1,19 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.13;
+pragma abicoder v2;
 
-import "../lib/solmate/src/auth/Owned.sol";
-import "../lib/solmate/src/utils/SafeTransferLib.sol";
-import "../lib/solmate/src/utils/ReentrancyGuard.sol";
+import "solmate/auth/Owned.sol";
+import "solmate/utils/SafeTransferLib.sol";
 
 error Unauthorized();
 error UncleBlock();
 error SelfCall();
 error SufficientIncome();
+error FlashLenderCall();
 error CoinbaseCall();
 
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
 }
 
 interface IERC3156FlashBorrower {
@@ -35,8 +39,47 @@ interface IERC3156FlashBorrower {
     ) external returns (bytes32);
 }
 
-contract Arbitrage is Owned, ReentrancyGuard, IERC3156FlashBorrower {
+interface IERC3156FlashLender {
+
+    /**
+     * @dev The amount of currency available to be lent.
+     * @param token The loan currency.
+     * @return The amount of `token` that can be borrowed.
+     */
+    function maxFlashLoan(
+        address token
+    ) external view returns (uint256);
+
+    /**
+     * @dev The fee to be charged for a given loan.
+     * @param token The loan currency.
+     * @param amount The amount of tokens lent.
+     * @return The amount of `token` to be charged for the loan, on top of the returned principal.
+     */
+    function flashFee(
+        address token,
+        uint256 amount
+    ) external view returns (uint256);
+
+    /**
+     * @dev Initiate a flash loan.
+     * @param receiver The receiver of the tokens in the loan, and the receiver of the callback.
+     * @param token The loan currency.
+     * @param amount The amount of tokens lent.
+     * @param data Arbitrary data structure, intended to contain user-defined parameters.
+     */
+    function flashLoan(
+        IERC3156FlashBorrower receiver,
+        address token,
+        uint256 amount,
+        bytes calldata data
+    ) external returns (bool);
+}
+
+contract Arbitrage is Owned, IERC3156FlashBorrower {
     using SafeTransferLib for IERC20;
+
+    address flashLender;
 
     constructor() Owned(msg.sender) {}
 
@@ -46,33 +89,48 @@ contract Arbitrage is Owned, ReentrancyGuard, IERC3156FlashBorrower {
         _;
     }
 
+    function setFlashLender(address _lender) public onlyOwner {
+        flashLender = _lender;
+    }
+
+    function _arbitrage(bytes calldata data) internal {
+        uint256 balance_before = address(this).balance;
+
+        // parse data
+        (bytes32 _parentHash, uint256 _coinbaseFee, bytes[] memory _multicallData) = abi.decode(data, (bytes32, uint256, bytes[]));
+        if ((_parentHash != bytes32("")) && (blockhash(block.number - 1) != _parentHash)) revert UncleBlock();
+
+        // multicall
+        for (uint256 i = 0; i < _multicallData.length; i++) {
+            (address _to, uint256 _value, bytes memory _data) = abi.decode(_multicallData[i], (address, uint256, bytes));
+            _to.call{ value: _value }(_data);
+        }
+
+        block.coinbase.transfer(_coinbaseFee);
+        uint256 balance_after = address(this).balance;
+        // onFlashLoan only checks the ETH balance, not ERC20.
+        // Recommend switch to ETH after each arbitrage.
+        if (balance_after <= balance_before) revert SufficientIncome();
+    }
+
+    function run(bytes calldata data) external onlyOwner {
+        _arbitrage(data);
+    }
+
     function onFlashLoan(
         address initiator,
         address token,
         uint256 amount,
         uint256 fee,
         bytes calldata data
-    ) external nonReentrant returns (bytes32) {
-        if (tx.origin != owner) revert Unauthorized();
-        uint256 balance_before = address(this).balance;
+    ) public returns (bytes32) {
+        if (msg.sender != flashLender) revert FlashLenderCall();
+        if (initiator != owner) revert Unauthorized();
 
-        (bytes32 _parentHash, uint256 _coinbaseFee, bytes[] memory _multicallData) = abi.decode(data, (bytes32, uint256, bytes[]));
-        if ((_parentHash != bytes32("")) && (blockhash(block.number - 1) != _parentHash)) revert UncleBlock();
-        // multicall
-        for (uint256 i = 0; i < _multicallData.length; i++) {
-            (address _to, uint256 _value, bytes memory _data) = abi.decode(_multicallData[i], (address, uint256, bytes));
-            if (_to == address(this)) revert SelfCall();
-            _to.call{ value: _value }(_data);
-        }
+        uint256 _allowance = IERC20(token).allowance(address(this), msg.sender);
+        IERC20(token).approve(msg.sender, _allowance + amount + fee);
 
-        // to show reentrant attack, if only consider EOA coinbase, use `transfer` instead
-        (bool _success, ) = block.coinbase.call{ value: _coinbaseFee, gas: 100000 }("");
-        if(!_success) revert CoinbaseCall();
-
-        uint256 balance_after = address(this).balance;
-        // onFlashLoan only checks the ETH balance, not ERC20.
-        // Recommend switch to ETH after each arbitrage.
-        if (balance_after <= balance_before) revert SufficientIncome();
+        _arbitrage(data);
 
         return keccak256("ERC3156FlashBorrower.onFlashLoan");
     }
@@ -85,5 +143,5 @@ contract Arbitrage is Owned, ReentrancyGuard, IERC3156FlashBorrower {
         IERC20(token).transfer(msg.sender, IERC20(token).balanceOf(address(this)));
     }
 
-    fallback() external payable {}
+    receive() external payable {}
 }
--- test/Arbitrage.t.sol
+++ test/Arbitrage.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 pragma abicoder v2;
 
-import "../lib/forge-std/src/Test.sol";
+import "forge-std/Test.sol";
 import "../src/Arbitrage.sol";
 
 contract ArbitrageTest is Test {
     Arbitrage public arbitrage;
     FakeERC20 public fakeERC20;
-    EvilCoinbase public evilCoinbase;
+    FlashLender public flashLender;
 
     modifier TestCannotOperateByNotOwner() {
         vm.expectRevert(Unauthorized.selector);
@@ -20,69 +20,52 @@ contract ArbitrageTest is Test {
     function setUp() public {
         arbitrage = new Arbitrage();
         fakeERC20 = new FakeERC20("FakeERC20", "Fake", 18);
-        evilCoinbase = new EvilCoinbase();
+        flashLender = new FlashLender();
+
+        vm.deal(address(flashLender), 1000 ether);
+        vm.prank(address(flashLender));
+        address(fakeERC20).call{value: 1000 ether}(abi.encodeWithSignature("deposit()", ""));
+        arbitrage.setFlashLender(address(flashLender));
     }
 
-    function testCannotFlashByNotOwner() public TestCannotOperateByNotOwner {
-        arbitrage.onFlashLoan(address(0), address(0), 0, 0, abi.encode(
-            bytes32(""), uint256(0), [abi.encode(address(0), uint256(0), bytes(""))]
-        ));
+    function testCannotArbitrageByNotOwner() public TestCannotOperateByNotOwner {
+        arbitrage.run(abi.encode(bytes32(""), 0, bytes("")));
     }
-    function testCannotFlashByUncleBlock() public {
+    function testCannotArbitrageByUncleBlock() public {
         vm.expectRevert(UncleBlock.selector);
         vm.roll(3);
-        vm.prank(address(this), address(this));
-
-        bytes[] memory payloads = new bytes[](1);
-        payloads[0] = abi.encode(address(0), uint256(0), bytes(""));
-        arbitrage.onFlashLoan(address(0), address(0), 0, 0, abi.encode(blockhash(block.number - 2), uint256(0), payloads));
+        arbitrage.run(abi.encode(blockhash(block.number - 2), 0, bytes("")));
     }
-    function testCannotFlashBySelfCall() public {
-        vm.expectRevert(SelfCall.selector);
-        vm.prank(address(this), address(this));
-
-        bytes[] memory payloads = new bytes[](1);
-        payloads[0] = abi.encode(address(arbitrage), uint256(0), bytes(""));
-        arbitrage.onFlashLoan(address(0), address(0), 0, 0, abi.encode(bytes32(""), uint256(0), payloads));
-    }
-    function testCannotFlashBySufficientIncome() public {
+    function testCannotArbitrageBySufficientIncome() public {
         vm.expectRevert(SufficientIncome.selector);
-        vm.prank(address(this), address(this));
-
-        bytes[] memory payloads = new bytes[](1);
-        payloads[0] = abi.encode(address(0), uint256(0), bytes(""));
-        arbitrage.onFlashLoan(address(0), address(0), 0, 0, abi.encode(bytes32(""), uint256(0), payloads));
+        arbitrage.run(abi.encode(bytes32(""), 0, bytes("")));
     }
-    function testCannotFlashByReentrancy() public {
-        // ERC20 Asset
-        fakeERC20.mint(address(arbitrage), 1 ether);
-        evilCoinbase.setToken(address(fakeERC20));
+    function testFlashArbitrageByOwner() public {
+        uint256 _lenderAmount = 1000 ether;
 
-        // Bribe Fund
-        vm.deal(address(evilCoinbase), 1 wei);
-        vm.deal(address(fakeERC20), 1 ether);
-        vm.coinbase(address(evilCoinbase));
-        vm.prank(address(this), address(this));
-        vm.expectRevert(CoinbaseCall.selector);
+        bytes[] memory payloads = new bytes[](3);
+        payloads[0] = abi.encode(address(fakeERC20), 0, abi.encodeWithSignature(
+            "transferFrom(address,address,uint256)",
+            address(flashLender), address(arbitrage), _lenderAmount
+        ));
+        payloads[1] = abi.encode(address(fakeERC20), 0, abi.encodeWithSignature("exploit()", ""));
+        payloads[2] = abi.encode(address(fakeERC20), 0, abi.encodeWithSignature("withdraw(uint256)", 1 ether));
 
-        bytes[] memory payloads = new bytes[](1);
-        payloads[0] = abi.encode(address(fakeERC20), uint256(0), abi.encodeWithSignature("exploit()", ""));
-        arbitrage.onFlashLoan(address(0), address(0), 0, 0, abi.encode(blockhash(block.number - 1), uint256(0), payloads));
-        // assertEq(fakeERC20.balanceOf(address(evilCoinbase)), 1 ether);
-    }
-    function testFlashByOwner() public {
-        vm.deal(address(arbitrage), 0 ether);
-        vm.deal(address(fakeERC20), 2 ether);
-        vm.prank(address(this), address(this));
-
-        bytes[] memory payloads = new bytes[](2);
-        payloads[0] = abi.encode(address(fakeERC20), uint256(0), abi.encodeWithSignature("exploit()", ""));
-        payloads[1] = abi.encode(address(fakeERC20), uint256(0), abi.encodeWithSignature("exploit()", ""));
-        assertEq(
-            arbitrage.onFlashLoan(address(0), address(0), 0, 0, abi.encode(blockhash(block.number - 1), uint256(0), payloads)),
-            keccak256("ERC3156FlashBorrower.onFlashLoan")
+        assertEq(address(arbitrage).balance, 0 ether);
+        assertEq(fakeERC20.balanceOf(address(arbitrage)), 0 ether);
+        flashLender.flashLoan(
+            arbitrage, address(fakeERC20), _lenderAmount,
+            abi.encode(blockhash(block.number - 1), 0, payloads)
         );
-        assertEq(address(arbitrage).balance, 2 ether);
+        assertEq(address(arbitrage).balance, 1 ether);
+        assertEq(fakeERC20.balanceOf(address(arbitrage)), 0 ether);
+    }
+
+    function testSetFlashLenderByOwner() public {
+        arbitrage.setFlashLender(address(0));
+    }
+    function testCannotSetFlashLenderByNotOwner() public TestCannotOperateByNotOwner {
+        arbitrage.setFlashLender(address(0));
     }
 
     function testWithdrawByOwner() public {
@@ -111,41 +94,44 @@ contract ArbitrageTest is Test {
 }
 
 contract FakeERC20 is ERC20, Owned {
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol, _decimals) Owned(msg.sender) {
-        _mint(msg.sender, 1 ether);
-    }
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol, _decimals) Owned(msg.sender) {}
 
     function exploit() external {
-        payable(msg.sender).transfer(1 ether);
+        if (balanceOf[msg.sender] >= 1000 ether) {
+            _mint(msg.sender, 2 ether);
+        }
     }
 
-    function mint(address _to, uint256 _amount) public onlyOwner {
-        _mint(_to, _amount);
+    function deposit() payable external {
+        _mint(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 amount) external {
+        _burn(msg.sender, amount);
+        payable(msg.sender).transfer(amount);
     }
 }
 
-contract EvilCoinbase {
-    uint256 private _once;
-    address private _token;
-
-    function setToken(address t) external {
-        _token = t;
+contract FlashLender is IERC3156FlashLender {
+    function maxFlashLoan(address token) public view returns (uint256) {
+        return IERC20(token).balanceOf(address(this));
     }
 
-    fallback() external payable {
-        if (_once < block.number) {
-            _once = block.number;
-            // Only ERC tokens can be attacked
-            IERC3156FlashBorrower(msg.sender).onFlashLoan(address(0), address(0), 0, 0, abi.encode(
-                bytes32(""), uint256(0), [abi.encode(
-                    address(_token), uint256(0), abi.encodeWithSignature(
-                        "transfer(address,uint256)",
-                        address(this), IERC20(_token).balanceOf(msg.sender)
-                    )
-                )]
-            ));
-        } else {
-            payable(msg.sender).transfer(1 wei);
-        }
+    function flashFee(address token, uint256 amount) public view returns (uint256) {
+        return amount / 1000;
+    }
+
+    function flashLoan(IERC3156FlashBorrower receiver, address token, uint256 amount, bytes calldata data) external returns (bool) {
+        require(amount <= maxFlashLoan(token), "FlashLender: Insufficient funds");
+        uint256 fee = flashFee(token, amount);
+        IERC20(token).approve(address(receiver), amount);
+        require(
+            receiver.onFlashLoan(msg.sender, token, amount, fee, data) == keccak256("ERC3156FlashBorrower.onFlashLoan"),
+            "FlashMinter: Do not support IERC3156FlashBorrower"
+        );
+        uint256 _allowance = IERC20(token).allowance(address(receiver), address(this));
+        require(_allowance >= (amount + fee), "FlashMinter: Repay not approved");
+        IERC20(token).transferFrom(address(receiver), address(this), amount + fee);
+        return true;
     }
 }
