b08fe74f4355de37fc742f3ed172067688b5819a
YuFei Wang <zhongliwang48@gmail.com>
2023-01-15 11:29:22 +0800

    style: log_profit

--- src/utils/base.rs
+++ src/utils/base.rs
@@ -1,4 +1,5 @@
 use ethers::prelude::*;
+use ethers::utils::format_units;
 use std::env;
 use std::future::Future;
 
@@ -15,14 +16,19 @@ pub async fn log_profit<M: Middleware, S: Signer, Fut: Future<Output = ()>, F: F
 ) {
     println!("\n--------------- Simulate {tx_hash:?} ---------------");
     let balance_before = client.get_balance(address, None).await.unwrap();
-
     handle().await;
-
     let balance_after = client.get_balance(address, None).await.unwrap();
+
+    let profit = format_units(profit, "eth").unwrap();
+    let balance_diff = format_units(balance_after - balance_before, "eth").unwrap();
+    let balance_before = format_units(balance_before, "eth").unwrap();
+    let balance_after = format_units(balance_after, "eth").unwrap();
+
     println!("");
-    println!("Expected profit: {profit:?}");
-    println!("Balance before:  {:?} {:?}", address, balance_before);
-    println!("Balance after:   {:?} {:?}", address, balance_after);
-    println!("Balance diff:    {:?}", balance_after - balance_before);
+    println!("Address:         {address:?}");
+    println!("Expected profit: {profit:.6} eth");
+    println!("Balance before:  {balance_before:.6} eth");
+    println!("Balance after:   {balance_after:.6} eth");
+    println!("Balance diff:    {balance_diff:.6} eth");
     println!("----------------{:-^75}----------------\n", "");
 }
