b6df3ee92c20fa6ee1386a7affcc4808a8d8c2e8
YuFei Wang <zhongliwang48@gmail.com>
2023-01-20 15:59:53 +0800

    refactor: support multi chains flation token check

--- script/NotFlationToken.s.sol
+++ script/NotFlationToken.s.sol
@@ -5,13 +5,6 @@ import "forge-std/Test.sol";
 import "contract//Arbitrage.sol";
 
 contract NotFlationTokenScript is Test {
-    // 0xa806617cdd8ed760ed25cec61abf642f4889749c3cede45c46f27d60f0941bd1
-    address public constant QTN = 0xC9fa8F4CFd11559b50c5C7F6672B9eEa2757e1bd;
-    // 0xd099a41830b964e93415e9a8607cd92567e40d3eeb491d52f3b66eee6b0357eb
-    address public constant UPStkn = 0xFFeE5EcDE135a7b10A0Ac0E6e617798e6aD3D0D6;
-
-    address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-
     struct Token {
         address addr;
         address lp;
@@ -19,16 +12,25 @@ contract NotFlationTokenScript is Test {
 
     mapping (address => bool) public notFlationTokenMap;
 
-    function setUp() public {
+    function run() public {
         vm.createSelectFork("https://rpc.ankr.com/eth", 16428369);
+        run_eth();
+        vm.createSelectFork("https://rpc.ankr.com/bsc", 24912666);
+        run_bsc();
     }
 
-    function run() public {
+    function run_eth() public {
+        // 0xa806617cdd8ed760ed25cec61abf642f4889749c3cede45c46f27d60f0941bd1
+        address QTN = 0xC9fa8F4CFd11559b50c5C7F6672B9eEa2757e1bd;
+        // 0xd099a41830b964e93415e9a8607cd92567e40d3eeb491d52f3b66eee6b0357eb
+        address UPStkn = 0xFFeE5EcDE135a7b10A0Ac0E6e617798e6aD3D0D6;
+        address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
         Token[3] memory TOKEN_LIST = [
             Token(QTN, 0xA8208dA95869060cfD40a23eb11F2158639c829B),
             Token(UPStkn, 0xa3f47DCFC09d9aaDb7Ac6ECAB257cf7283BFEe26),
             Token(WETH, 0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc)
         ];
+
         for (uint i = 0;i < TOKEN_LIST.length; i++) {
             // ignore error
             address(this).call(abi.encodeWithSignature("check((address,address))", TOKEN_LIST[i]));
@@ -37,11 +39,32 @@ contract NotFlationTokenScript is Test {
         // Unable to detect irregular contracts
         assert(notFlationTokenMap[QTN] == false);
 
-        // lp pool
+        // transfer self increment
         assert(notFlationTokenMap[UPStkn] == true);
 
         // weth is safety
         assert(notFlationTokenMap[WETH] == false);
+    }
+
+    function run_bsc() public {
+        // 0x5058c820fa0bb0daff2bd1b30151cf84c618dffe123546223b7089c8c2e18331
+        address THOREUM = 0x79Fe086a4C03C5E38FF8074DEA9Ee0a18dC1AF4F;
+        address WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+        Token[2] memory TOKEN_LIST = [
+            Token(THOREUM, 0xd822E1737b1180F72368B2a9EB2de22805B67E34),
+            Token(WBNB, 0x1CEa83EC5E48D9157fCAe27a19807BeF79195Ce1)
+        ];
+
+        for (uint i = 0;i < TOKEN_LIST.length; i++) {
+            // ignore error
+            address(this).call(abi.encodeWithSignature("check((address,address))", TOKEN_LIST[i]));
+        }
+
+        // not a simple transfer self increment, the specific arbitrage logic is not clear
+        assert(notFlationTokenMap[THOREUM] == false);
+
+        // wbnb is safety
+        assert(notFlationTokenMap[WBNB] == false);
     }
 
     function check(Token calldata _token) external {
@@ -61,6 +84,8 @@ contract NotFlationTokenScript is Test {
         _transfer(_token, address(lp), address(this), amount);
         // UPStkn exploit, transfer self with zero amount
         _transfer(_token, address(this), address(this), 0);
+        // THOREUM exploit, transfer self with non-zero amount
+        _transfer(_token, address(this), address(this), amount);
     }
 
     function _transfer(Token calldata _token, address from, address to, uint256 amount) internal {
